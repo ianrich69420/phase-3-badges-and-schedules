779aa12a33be948a9493ee24a8f866b04af408ea
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-19 20:28:51 +0300

    Completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map { |value| "Hello, my name is #{value}." }
+end
+
+def assign_rooms array
+    array.map { |value| "Hello, #{value}! You'll be assigned to room #{array.index(value) + 1}!" }
+end
+
+def printer array
+    batch_badge_creator(array).each { |value| puts value }
+    assign_rooms(array).each { |value| puts value}
+end
