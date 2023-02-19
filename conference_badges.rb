75e80aadfeff704424ab95b2b0a015b744cefa0f
ianrich69420 <ian.orieko@student.moringaschool.com>
2023-02-19 20:27:41 +0300

    commit tree 02c422cf14966f5323384247091bbf032cf352cc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array
+    array.map { |value| "Hello, my name is #{value}." }
+end
+
+def assign_rooms array
+    array.map { |value| "Hello, #{value}! You'll be assigned to room #{array.index(value) + 1}!" }
+end
+
+def printer array
+    batch_badge_creator(array).each { |value| puts value }
+    assign_rooms(array).each { |value| puts value}
+end
+
+binding.pry
+0
